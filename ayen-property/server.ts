edae553058d305672859c6d06ec9d55cf6fd4738
Dinand <ferdinandarya80@gmail.com>
2026-03-06 23:03:44 +0700

    Update server.ts

--- ayen-property/server.ts
+++ ayen-property/server.ts
@@ -122,7 +122,7 @@ async function startServer() {
     app.use(express.static('dist'));
   }
 
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
   app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
   });
